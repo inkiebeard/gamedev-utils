1f6842d7e3bb45c27f8ba4d742874ed78968c79e
Aaron Toomey <toomey.aaron@gmail.com>
2021-01-10 21:50:05 +1100

    add namespace

--- simpleShoot.cs
+++ simpleShoot.cs
@@ -1,19 +1,21 @@
 using UnityEngine;
 
-public class simpleShoot : MonoBehaviour {
-    public Transform bullet;
-    public float speed = 1000;
-    public Transform firePoint;
-    public float lifetime = 3f;
+namespace InkiebeardGamedevUtils {
+    public class simpleShoot : MonoBehaviour {
+        public Transform bullet;
+        public float speed = 1000;
+        public Transform firePoint;
+        public float lifetime = 3f;
 
-    void Start() {
-        firePoint = firePoint ? firePoint : GameObject.Find("PlayerFirePoint").transform;
-    }
-    void Update () {
-        if (Input.GetMouseButtonDown(0)) {
-            Transform bl = Instantiate(bullet, firePoint.position, firePoint.rotation);
-            bl.GetComponent<Rigidbody>().AddForce(bl.forward * speed);
-            Destroy(bl.gameObject,lifetime);
+        void Start() {
+            firePoint = firePoint ? firePoint : GameObject.Find("PlayerFirePoint").transform;
+        }
+        void Update () {
+            if (Input.GetMouseButtonDown(0)) {
+                Transform bl = Instantiate(bullet, firePoint.position, firePoint.rotation);
+                bl.GetComponent<Rigidbody>().AddForce(bl.forward * speed);
+                Destroy(bl.gameObject,lifetime);
+            }
         }
     }
 }
